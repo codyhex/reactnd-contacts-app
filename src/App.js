e1cc4b13ac072e0e0c3e893d452d2cb6be57b29b
Peng He <thinkinbath@gmail.com>
2020-05-07 17:04:05 -0400

    自己改了一下，但是还是不对啊

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import * as ContactsAPI from './utils/ContactsAPI';
+// 原来还可以把所有的 function 这样包装一下变成 API，赞
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      age: ''
+    };
+  }
+
+  componentDidMount() {
+    ContactsAPI.getAll().then((user) => this.setState({
+      name: user.name,
+      age: user.age
+    }));
+  }
+
   render() {
     return (
-      <div>
-        Hello World
-      </div>
+        <div>
+          <p>Name: {this.state.name}</p>
+          <p>Age: {this.state.age}</p>
+        </div>
     );
   }
 }
 
 export default App;
+
